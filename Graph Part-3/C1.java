a522c6534ed601b542ad75ad983149175b205aff
KAIN <kshitijjain6074@gmail.com>
2025-05-20 16:34:09 +0530

    update

--- Graph Part-3/C1.java
+++ Graph Part-3/C1.java
@@ -32,47 +32,59 @@ public class C1 {
     }
 
     static class Pair implements Comparable<Pair> {
-        int n;
-        int path;
+        int n;  //node
+        int path; //shortest path int
 
-        public Pair(int n, int path) {
-            this.n =n;
-            this.path =path;
+        public Pair(int n, int path) { //constructor
+            this.n = n;
+            this.path = path;
         }
 
         @Override  //comparTo fxn ko override karna padta hai , because abhi tak object ko compare karne k parameters[node or path] nhi bataye
         public int compareTo(Pair p2) {
-            return this.path=p2.path; //jo value small hogi vo pehele return; path based sorting for pairs
+            return this.path - p2.path; //jo value small hogi vo pehele return; path based sorting for pairs(increasing order)
         }
     }
 
 
     public static void dijkstra(ArrayList<Edge> graph[], int src) {
-        int dist[] = new int[graph.length];  //dist[i] -> src to 1
+        int dist[] = new int[graph.length];  //dist[i] -> src to i
         for(int i=0; i<graph.length; i++) {
             if(i!=src) {
-                dist[i] = Integer.MAX_VALUE;  //INFINITY
+                dist[i] = Integer.MAX_VALUE;  //initialze all to INFINITY, ex: src
             }
         }
 
-        boolean vis[] = new boolean[graph.length];
+        boolean vis[] = new boolean[graph.length]; //all initially not visited
+
         PriorityQueue<Pair> pq = new PriorityQueue<>();
-        pq.add(new Pair(src, 0));
-        //loop
-        while(!pq.isEmpty()) {
-            Pair curr = pq.remove();
-            if(!vis[curr.n]) {
+        pq.add(new Pair(src, 0));  //first pair
+        //loop-BFS
+        while(!pq.isEmpty()) { 
+            Pair curr = pq.remove(); //shortest dist pair get removed first
+            if(!vis[curr.n]) {  //only for not visited
                 vis[curr.n] = true;
                 //neighbour
-                for(int i=0; i<graph[curr.n].size(); i++){
+                for(int i=0; i<graph[curr.n].size(); i++){   // curr is pair value, curr.n have idx stored which is req. here
                     Edge e = graph[curr.n].get(i);
-                    int u= e.src;
-                    int v= e.dest;
+                    int u = e.src;
+                    int v = e.dest;
                     int wt = e.wt;
+
+                    if(dist[u] + wt < dist[v]){ //update dist of src to v
+                        dist[v]  = dist[u] + wt;
+                        pq.add(new Pair(v, dist[v])); //add to queue
+                    }
                 }
             }
 
         }
+
+        //print all src to vertices shortest path
+        for(int i =0; i<dist.length; i++){
+            System.out.print(dist[i] + " ");
+        }
+        System.out.println();
 
     }
 
@@ -80,6 +92,9 @@ public class C1 {
         int V=6;
         ArrayList<Edge> graph[] = new ArrayList[V];
         createGraph(graph);
+
+        int src = 0;
+        dijkstra(graph, src);
     }
 }
 
