dc045e0ee9dfd911084ac9de042413d586533863
KAIN <kshitijjain6074@gmail.com>
2025-07-05 10:45:17 +0530

    Median of two Sorted Arrays of Different Sizes

--- Binary Search/U.java
+++ Binary Search/U.java
@@ -71,6 +71,31 @@ public static double median2(int[] a, int[] b){
 }
 
     //Approach-3: Binary Search
-    // TC = O(), SC = O(1)
+    // TC = O(log(min(n1, n2))), SC = O(1)
+public static double median3(int[] a, int[] b){
+    int n1 = a.length, n2 = b.length;
+    if(n1 > n2) return median3(b,a);  //if n1 if bigger swap array, arr1 must be smaller
 
+    int n = n1 + n2;
+    int left = (n1 + n2 + 1)/2;  //len of left half for both odd and even
+    
+    int low = 0, high = n1;
+    while(low <= high){
+        int mid1 = (low + high)/2;  //x
+        int mid2 = left - mid1;   //left-x
+
+        int l1 = (mid1 > 0) ? a[mid1 - 1] : Integer.MIN_VALUE;
+        int l2 = (mid2 > 0) ? b[mid2 - 1] : Integer.MIN_VALUE;
+        int r1 = (mid1 < n1) ? a[mid1] : Integer.MAX_VALUE;
+        int r2 = (mid2 < n2) ? b[mid2] : Integer.MAX_VALUE;
+
+        if(l1 <= r2 && l2 <= r1){ //answer found 
+            if(n % 2 == 1) return Math.max(l1, l2);
+            else return ((double) (Math.max(l1, l2) + Math.min(r1, r2))) /2.0;
+        }       //eliminating search sapce 
+        else if(l1 > r2) high = mid1 -1;
+        else low = mid1 + 1;
+    }
+    return 0;
+   }
 }
